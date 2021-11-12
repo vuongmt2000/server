dbc9767f9e80635aced25cd50a332e05e3da6874
vuong <vuongmt>
2021-11-12 15:30:12 +0700

    fix

--- routes/user.js
+++ routes/user.js
@@ -1,29 +1,25 @@
 const express = require('express');
 const router = express.Router()
-const userController =  require('../controllers/user.controller') 
+const userController = require('../controllers/user.controller')
 const multer = require('multer');
 const storage = multer.diskStorage({});
 const fileFilter = (req, file, cb) => {
     if (file.mimetype.startsWith('image')) {
-      cb(null, true);
+        cb(null, true);
     } else {
-      cb('invalid image file!', false);
+        cb('invalid image file!', false);
     }
-  };
+};
 const uploads = multer({ storage, fileFilter });
 // @route get data
 // @disc get user
 // @access public
 
-router.post('/', userController.get_user )
+router.post('/', userController.get_user)
 
 
 router.put('/update_user', userController.update_profile)
-router.post(
-    '/upload-profile',
-    uploads.single('profile'),
-    userController.uploadProfile
-  );
+router.post('/upload-profile',uploads.single('profile'),userController.uploadProfile);
 
 
 module.exports = router
